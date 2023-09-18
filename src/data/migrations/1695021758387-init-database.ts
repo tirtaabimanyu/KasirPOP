117ccb0fd4592ca8f8fed062fc4d64180637f2e4
tirtaabimanyu <tirtaabimanyu@gmail.com>
2023-09-18 14:29:28 +0700

    reset migrations

---
+++ src/data/migrations/1695021758387-init-database.ts
@@ -0,0 +1,73 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitDatabase1695021758387 implements MigrationInterface {
+  name = "InitDatabase1695021758387";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "products" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL, "stock" integer NOT NULL, "isAlwaysInStock" boolean NOT NULL, "price" integer NOT NULL, "imgUri" varchar)`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "categories" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar NOT NULL)`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "transactions" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "created_at" datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP), "total_price" integer NOT NULL, "products" text NOT NULL)`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "products_categories_categories" ("productsId" integer NOT NULL, "categoriesId" integer NOT NULL, PRIMARY KEY ("productsId", "categoriesId"))`
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_40e7da0284a5389344605de8da" ON "products_categories_categories" ("productsId") `
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_e1d833224b5be535323207473f" ON "products_categories_categories" ("categoriesId") `
+    );
+    await queryRunner.query(`DROP INDEX "IDX_40e7da0284a5389344605de8da"`);
+    await queryRunner.query(`DROP INDEX "IDX_e1d833224b5be535323207473f"`);
+    await queryRunner.query(
+      `CREATE TABLE "temporary_products_categories_categories" ("productsId" integer NOT NULL, "categoriesId" integer NOT NULL, CONSTRAINT "FK_40e7da0284a5389344605de8dab" FOREIGN KEY ("productsId") REFERENCES "products" ("id") ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT "FK_e1d833224b5be535323207473f1" FOREIGN KEY ("categoriesId") REFERENCES "categories" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION, PRIMARY KEY ("productsId", "categoriesId"))`
+    );
+    await queryRunner.query(
+      `INSERT INTO "temporary_products_categories_categories"("productsId", "categoriesId") SELECT "productsId", "categoriesId" FROM "products_categories_categories"`
+    );
+    await queryRunner.query(`DROP TABLE "products_categories_categories"`);
+    await queryRunner.query(
+      `ALTER TABLE "temporary_products_categories_categories" RENAME TO "products_categories_categories"`
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_40e7da0284a5389344605de8da" ON "products_categories_categories" ("productsId") `
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_e1d833224b5be535323207473f" ON "products_categories_categories" ("categoriesId") `
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "IDX_e1d833224b5be535323207473f"`);
+    await queryRunner.query(`DROP INDEX "IDX_40e7da0284a5389344605de8da"`);
+    await queryRunner.query(
+      `ALTER TABLE "products_categories_categories" RENAME TO "temporary_products_categories_categories"`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "products_categories_categories" ("productsId" integer NOT NULL, "categoriesId" integer NOT NULL, PRIMARY KEY ("productsId", "categoriesId"))`
+    );
+    await queryRunner.query(
+      `INSERT INTO "products_categories_categories"("productsId", "categoriesId") SELECT "productsId", "categoriesId" FROM "temporary_products_categories_categories"`
+    );
+    await queryRunner.query(
+      `DROP TABLE "temporary_products_categories_categories"`
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_e1d833224b5be535323207473f" ON "products_categories_categories" ("categoriesId") `
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_40e7da0284a5389344605de8da" ON "products_categories_categories" ("productsId") `
+    );
+    await queryRunner.query(`DROP INDEX "IDX_e1d833224b5be535323207473f"`);
+    await queryRunner.query(`DROP INDEX "IDX_40e7da0284a5389344605de8da"`);
+    await queryRunner.query(`DROP TABLE "products_categories_categories"`);
+    await queryRunner.query(`DROP TABLE "transactions"`);
+    await queryRunner.query(`DROP TABLE "categories"`);
+    await queryRunner.query(`DROP TABLE "products"`);
+  }
+}
--- src/data/options.ts
+++ src/data/options.ts
@@ -1,11 +1,9 @@
 import { ProductModel } from "./entities/ProductModel";
 import { CategoryModel } from "./entities/CategoryModel";
 import { TransactionModel } from "./entities/TransactionModel";
-
-import { Init1694777113671 } from "./migrations/1694777113671-init";
-import { AddTransactions1695021475234 } from "./migrations/1695021475234-add-transactions";
+import { InitDatabase1695021758387 } from "./migrations/1695021758387-init-database";
 
 export const CommonDataSourceOptions = {
   entities: [ProductModel, CategoryModel, TransactionModel],
-  migrations: [Init1694777113671, AddTransactions1695021475234],
+  migrations: [InitDatabase1695021758387],
 };
